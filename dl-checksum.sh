8e5255032c88d84053f1efc27846466fb47af3a9
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-01-19 15:37:56 -0500

    up to v1.4.2

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,32 +1,37 @@
 #!/usr/bin/env sh
-VER=1.1.0
 DIR=~/Downloads
-MIRROR=https://github.com/cyberark/secretless-broker/releases/download/v${VER}
-
-# https://github.com/cyberark/secretless-broker/releases/download/v1.1.0/SHA256SUMS.txt
-RSHAFILE=SHA256SUMS.txt
-LSHAFILE=$DIR/secretless-broker-${VER}-SHA256SUMS.txt
-
-if [ ! -e $LSHAFILE ];
-then
-    wget -q -O $LSHAFILE $MIRROR/$RSHAFILE
-fi
+MIRROR=https://github.com/cyberark/secretless-broker/releases/download
 
 dl()
 {
-    OS=$1
-    ARCH=$2
-    ARCHIVETYPE=$3
-    PLATFORM=${OS}_${ARCH}
-    FILE=secretless-broker_${VER}_${PLATFORM}.${ARCHIVETYPE}
-    URL=$MIRROR/$FILE
+    local ver=$1
+    local lshafile=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}_${arch}"
+    local file=secretless-broker_${ver}_${platform}.${archive_type}
+    local url=$MIRROR/v${ver}/$file
 
-    printf "    # %s\n" $URL
-    printf "    %s: sha256:%s\n" $PLATFORM `grep $FILE $LSHAFILE | awk '{print $1}'`
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform `grep $file $lshafile | awk '{print $1}'`
 }
 
-printf "  '%s':\n" $VER
-dl linux amd64 tar.gz
+dl_ver() {
+    local ver=$1
+    # https://github.com/cyberark/secretless-broker/releases/download/v1.1.0/SHA256SUMS.txt
+    local rshafile=
+    local lshafile=$DIR/secretless-broker-${ver}-SHA256SUMS.txt
+
+    if [ ! -e $lshafile ];
+    then
+        wget -q -O $lshafile $MIRROR/v$ver/SHA256SUMS.txt
+    fi
+
+    printf "  '%s':\n" $ver
+    dl $ver $lshafile linux amd64
+}
 
 
 
+dl_ver ${1:-1.4.2}
